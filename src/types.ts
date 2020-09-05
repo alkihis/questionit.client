36945901961dbb4ae01c057c105ecc3f1f3629d3
Alkihis <alkihis@gmail.com>
2020-09-05 22:23:20 +0200

    Added more typings

--- src/types.ts
+++ src/types.ts
@@ -5,19 +5,75 @@ export interface AccessTokenResult {
   user: SentUser;
 }
 
+export interface PollResult {
+  /** Poll ID to specify in POST questions endpoints. */
+  poll_id: string;
+  /** UNIX timestamp until poll ID stays valid. */
+  until: number;
+}
+
+export interface QuestionTree {
+  /** Sorted ancestors. First one is the oldest one. */
+  ancestors: SentQuestion[];
+  /** Root question. */
+  question: SentQuestion;
+}
+
+export interface TokenVerifyResult {
+  user: SentUser;
+  rights: { [name: string]: 'true' | 'false' };
+}
+
+export interface CursoredLikes<T> {
+  /** previous is a cursor meant to be used as "since" parameter to get the newer ones */
+  previous_cursor: string,
+  /** next is a cursor meant to be used as "until" parameter to get likes made before returned ones */
+  next_cursor: string,
+  /** `SentUser` of likers */
+  likes: T[],
+} 
+
+export interface CursoredFollowers {
+  /** previous is a cursor meant to be used as "since" parameter to get the newer ones */
+  previous_cursor: string,
+  /** next is a cursor meant to be used as "until" parameter to get likes made before returned ones */
+  next_cursor: string,
+  /** `SentUser` array of followers */
+  followers: SentUser[],
+} 
+
+export interface CursoredFollowings {
+  /** previous is a cursor meant to be used as "since" parameter to get the newer ones */
+  previous_cursor: string,
+  /** next is a cursor meant to be used as "until" parameter to get likes made before returned ones */
+  next_cursor: string,
+  /** `SentUser` array of followings */
+  followings: SentUser[],
+} 
+
+export interface QuestionNotificationCount {
+  /** Awaiting question count */
+  question_count: number;
+  /** Unseen notification count */
+  notification_count: number;
+}
 
 /* GENERIC TYPES RETURNED BY API */
 
 export type NotificationType = 'answered' | 'question' | 'follow' | 'follow-back';
 
 export interface SentNotification {
+  /** Notification ID */
   id: string;
+  /** ISO date of notification creation */
   created_at: string;
+  /** If the notification has been seen by the user */
   seen: boolean;
+  /** Notification typeo */
   type: NotificationType,
   /** Defined if {type} is 'answered' or 'question' */
   question?: SentQuestion,
-  // Si follow: {type} is 'follow' or 'follow-back'.
+  /** If follow: {type} is 'follow' or 'follow-back'. */
   user?: SentUser;
 }
 
@@ -27,8 +83,11 @@ export interface WrappedApiError {
 }
 
 export interface QuestionItApiError {
+  /** API error code (have a correspondance in QuestionItApiErrors enumeration) */
   code: number;
+  /** Human-readable error message */
   message: string;
+  /** HTTP status code, duplicated */
   status_code: number;
 }
 
@@ -93,60 +152,102 @@ export enum QuestionItApiErrors {
 }
 
 export interface SentUser {
+  /** User ID */
   id: string;
+  /** User conventional name */
   name: string;
+  /** User @ */
   slug: string;
+  /** Twitter user.id_str */
   twitter_id: string;
+  /** User message displayed on profile question box */
   ask_me_message: string;
+  /** User creation ISO date */
   created_at: string;
+  /** Absolute link to profile picture */
   profile_picture: string | null;
+  /** Absolute link to banner */
   banner_picture: string | null;
+  /** If user accepts anon questions or not */
   allow_anonymous: boolean;
-  default_send_twitter: boolean | null;
-  allow_question_of_the_day: boolean | null;
+  /** If user is visible in searches or not */
   visible: boolean;
-
-  // Advanced infos
-  pinned_question?: SentQuestion;
+  /** Number of questions */
   question_count: number;
+  /** Number of followers */
   follower_count: number;
+  /** Number of followings */
   following_count: number;
+  /** Pinned question (only sent by users/* endpoints) */
+  pinned_question?: SentQuestion;
+
+  // Personal infos
+  /** If user wants to send questions to Twitter automatically or not (null if this user is not you) */
+  default_send_twitter: boolean | null;
+  /** If user wants to see QOTD in their waiting question page (null if this user is not you) */
+  allow_question_of_the_day: boolean | null;
+  /** Your relationship with this user (only sent by numerous users/* endpoints) */
   relationship?: SentRelationship;
+  /** If user wants to auto-delete questions that match muted words (undefined if not you) */
   drop_on_block_match?: boolean;
+  /** If user wants to have safe mode (undefined if not you) */
   safe_mode?: boolean;
 }
 
 export interface SentQuestion {
+  /** Question ID */
   id: string;
+  /** User object of emitter if any, null if anonymous */
   emitter: SentUser | null;
+  /** User object of receiver */
   receiver: SentUser;
+  /** Number of likes */
   like_count: number;
+  /** Creation date of question */
   created_at: string;
+  /** Question text */
   content: string;
+  /** @deprecated unused */
   seen: boolean;
+  /** Answer text */
   answer: string | null;
-  answer_created_at: string;
+  /** Answser date (null if not answered) */
+  answer_created_at: string | null;
+  /** Question reply ID if this question is a reply, null otherwise */
   in_reply_to: string | null;
+  /** Number of replies */
   reply_count: number;
+  /** If this question has been liked by logged user (undefined if anonymous) */
   liked: boolean;
+  /** If the question is a QOTD */
   of_the_day: boolean;
+  /** Absolute path if an image is linked to this question, null otherwise */
   image: string | null;
+  /** true if GIF is present in `image` key, false otherwise */
   is_gif: boolean;
+  /** Question attachements (undefined if none) */
   attachements?: SentQuestionAttachements;
 }
 
 export interface SentQuestionAttachements {
+  /** Possible attached poll */
   poll?: SentPoll;
 }
 
 export interface SentPoll {
+  /** Poll ID */
   id: string;
+  /** Poll choices */
   options: string[];
 }
 
 export interface SentRelationship {
+  /** true if source user is following target user */
   following: boolean;
+  /** true if target user is following source user */
   followed_by: boolean;
+  /** true if source user has blocked target user */
   has_blocked: boolean;
+  /** true if target user has blocked source user */
   is_blocked_by: boolean;
 }
